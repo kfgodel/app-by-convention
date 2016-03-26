94e6fdf7fc845dec02ecc4fc66a4d938f07639b6
Dario L Garcia <kfgodel@gmail.com>
2016-03-26 15:29:14 -0300

    DLG-2345 Extract operation to its own project

---
+++ src/test/java/ar/com/kfgodel/appbyconvention/operation/AppTestContext.java
@@ -0,0 +1,37 @@
+package ar.com.kfgodel.appbyconvention.operation;
+
+import ar.com.dgarcia.javaspec.api.TestContext;
+import ar.com.kfgodel.appbyconvention.operation.api.ApplicationOperation;
+import ar.com.kfgodel.appbyconvention.operation.api.chains.ChainedSessionOperation;
+import ar.com.kfgodel.appbyconvention.operation.api.chains.ChainedTransactionOperation;
+import ar.com.kfgodel.appbyconvention.operation.api.chains.SessionScopedOperation;
+import ar.com.kfgodel.appbyconvention.operation.api.chains.TransactionScopedOperation;
+
+import java.util.function.Supplier;
+
+/**
+ * This type represents the test context used for procedures tests
+ * Created by kfgodel on 13/03/16.
+ */
+public interface AppTestContext extends TestContext {
+
+  ApplicationOperation operation();
+
+  void operation(Supplier<ApplicationOperation> definition);
+
+  SessionScopedOperation sessionScoped();
+
+  void sessionScoped(Supplier<SessionScopedOperation> definition);
+
+  TransactionScopedOperation transactionScoped();
+
+  void transactionScoped(Supplier<TransactionScopedOperation> definition);
+
+  ChainedSessionOperation<Integer[]> sessionChain();
+
+  void sessionChain(Supplier<ChainedSessionOperation<Integer[]>> definition);
+
+  ChainedTransactionOperation<Integer[]> transactionChain();
+
+  void transactionChain(Supplier<ChainedTransactionOperation<Integer[]>> definition);
+}
